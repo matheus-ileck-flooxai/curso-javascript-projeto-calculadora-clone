58ebf0cd5b2b5fe236797400c59d005eea6cbbb2
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-03-19 09:16:21 -0300

    Fix(CalcController): Fix error in dot calculation method

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -183,7 +183,7 @@ class CalcController{
 
                 let newValue = this.getLastOperation().toString() + value.toString();
 
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
             }
@@ -201,6 +201,8 @@ class CalcController{
     addDot(){
 
         let lastOperation = this.getLastOperation();
+
+        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
         if(this.isOperator(lastOperation) || !lastOperation){
             this.pushOperation('0.');
